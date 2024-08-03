8cc541b83384a8864a9abf12531712f96c7e4efb
Vadim Makhtarov <endervad5@gmail.com>
2024-08-03 03:17:07 +0300

    Update list up to release quests

--- lib/ConversionLists.psm1
+++ lib/ConversionLists.psm1
@@ -26,7 +26,7 @@ $global:QUEST_INCLUDE_LIST = @(
     'subfst027_00176', 'subfst049_00376', 'subfst056_00377', 'subfst058_00379',
     'subfst059_00380', 'subfst060_00381', 'subfst068_00384', 'subfst073_00387',
     'subfst055_00161', 'manfst006_00446', 'manfst007_00447', 'manfst008_00448',
-    'manfst009_00449', 'manfst200_00507', 'subwil141_00674',
+    'manfst009_00449', 'manfst200_00507', 'manfst201_00508', 'subwil141_00674',
     #    Limsa
     'mansea000_00541', 'mansea050_00542', 'openinglimsalominsa', 'mansea001_00107',
     'mansea002_00108', 'mansea003_00109', 'subsea001_00111', 'subsea050_00462',
@@ -35,6 +35,7 @@ $global:QUEST_INCLUDE_LIST = @(
     'subsea105_00402', 'subsea106_00403', 'subsea109_00406', 'subsea115_00412',
     'subsea118_00415', 'subsea116_00413', 'subsea117_00414', 'mansea006_00689',
     'mansea007_00544', 'mansea008_00690', 'mansea009_00545', 'mansea200_00546',
+    'mansea201_00526',
     #    Ul'dah
     'manwil000_00548', 'manwil050_00549', 'openinguldah', 'manwil001_00594',
     'manwil002_00568', 'manwil003_00569', 'manwil004_00570', 'subwil027_00595',
@@ -239,6 +240,165 @@ $global:QUEST_INCLUDE_LIST = @(
     'voiceman_04006',
     'stmbda701_02548', 'stmbda702_02549', 'stmbda703_02550', 'stmbda704_02551',
     'stmbda705_02552', 'stmbda706_02553',
+    #  4.1
+    'voiceman_04100',
+    'stmbdb101_02962', 'stmbdb102_02963', 'stmbdb103_02964', 'stmbdb104_02965',
+    'stmbdb105_02966', 'stmbdb106_02967', 'stmbdb107_02968', 'stmbdb108_02969',
+    'stmbdb109_02970', 'stmbdb110_02971', 'stmbdb111_02972',
+    #  4.2
+    'voiceman_04200',
+    'stmbdc101_03022', 'stmbdc102_03023', 'stmbdc103_03024', 'stmbdc104_03025',
+    'stmbdc105_03026', 'stmbdc106_03027', 'stmbdc107_03028', 'stmbdc108_03029',
+    #  4.3
+    'voiceman_04300',
+    'stmbdd101_03070', 'stmbdd102_03071', 'stmbdd103_03072', 'stmbdd104_03073',
+    'stmbdd105_03074', 'stmbdd106_03075', 'stmbdd107_03076',
+    #  4.4
+    'voiceman_04400',
+    'stmbde101_03143', 'stmbde102_03144', 'stmbde103_03145', 'stmbde104_03146',
+    'stmbde105_03147', 'stmbde106_03148', 'stmbde107_03149',
+    'voiceman_04401',
+    #  4.5
+    'voiceman_04500',
+    'stmbdf101_03179', 'stmbdf102_03180', 'stmbdf103_03181', 'stmbdf104_03182',
+    'stmbdf105_03183',
+    #  4.58
+    'voiceman_04505',
+    'stmbdg101_03184', 'stmbdg102_03185',
+    # 5.0 - Shadowbringers
+    'voiceman_05000',
+    'luckma101_03279', 'luckma102_03280', 'luckma103_03281', 'luckma201_03282',
+    'luckma202_03283', 'luckma203_03284', 'luckma204_03285', 'luckma205_03286',
+    'luckma206_03287', 'luckma207_03288', 'luckma208_03289', 'luckma209_03290',
+    'luckma301_03291', 'luckma302_03292', 'luckma303_03293', 'luckma304_03294',
+    'luckma305_03295', 'luckma306_03296', 'luckma307_03297', 'luckma308_03298',
+    'luckma309_03299', 'luckma401_03300', 'luckma402_03301',
+    'voiceman_05001',
+    'luckmb101_03302', 'luckmb102_03303', 'luckmb103_03304', 'luckmb104_03305',
+    'luckmb105_03306', 'luckmb106_03307', 'luckmb107_03308', 'luckmb108_03309',
+    'luckmb109_03310', 'luckmb110_03311', 'luckmb111_03312', 'luckmb112_03313',
+    'luckmb113_03314', 'luckmb114_03315', 'luckmb115_03316', 'luckmb116_03317',
+    'luckmb117_03318', 'luckmb118_03319', 'luckmb119_03320', 'luckmb120_03321',
+    'voiceman_05002',
+    'luckmc101_03322', 'luckmc102_03323', 'luckmc103_03324', 'luckmc104_03325',
+    'luckmc105_03326', 'luckmc106_03327', 'luckmc107_03328', 'luckmc108_03329',
+    'luckmc109_03330', 'luckmc110_03331', 'luckmc111_03332', 'luckmc112_03333',
+    'luckmc113_03334', 'luckmc114_03335', 'luckmc115_03336', 'luckmc116_03337',
+    'luckmc117_03338', 'luckmc118_03339', 'luckmc119_03340', 'luckmc120_03341',
+    'luckmc121_03342',
+    'voiceman_05003',
+    'luckmd101_03606', 'luckmd102_03607', 'luckmd103_03608', 'luckmd104_03609',
+    'luckmd105_03610', 'luckmd106_03611', 'luckmd107_03612', 'luckmd108_03613',
+    'luckmd109_03614', 'luckmd110_03615', 'luckmd111_03616', 'luckmd112_03617',
+    'luckmd113_03618', 'luckmd114_03619', 'luckmd115_03620', 'luckmd116_03621',
+    'luckmd117_03622',
+    'voiceman_05004',
+    'luckme101_03630', 'luckme102_03631', 'luckme103_03632', 'luckme104_03633',
+    'luckme105_03634', 'luckme106_03635', 'luckme107_03636', 'luckme108_03637',
+    'luckme109_03638', 'luckme110_03639', 'luckme111_03640', 'luckme112_03641',
+    'luckme113_03642', 'luckme114_03643', 'luckmf101_03644',
+    'voiceman_05005',
+    'luckmf102_03645', 'luckmf103_03646', 'luckmf104_03647', 'luckmf105_03648',
+    'luckmf106_03649', 'luckmf107_03650', 'luckmf108_03651', 'luckmf109_03652',
+    'luckmf110_03653', 'luckmf111_03654',
+    'voiceman_05006',
+    #  5.1
+    'voiceman_05100',
+    'luckmg101_03673', 'luckmg102_03674', 'luckmg103_03675', 'luckmg104_03676',
+    'luckmg105_03677', 'luckmg106_03678', 'luckmg107_03679', 'luckmg108_03680',
+    'luckmg109_03681', 'luckmg110_03682',
+    #  5.2
+    'voiceman_05200',
+    'luckmh101_03761', 'luckmh102_03762', 'luckmh103_03763', 'luckmh104_03764',
+    'luckmh105_03765', 'luckmh106_03766', 'luckmh107_03767', 'luckmh108_03768',
+    'luckmh109_03769', 'luckmh110_03770',
+    #  5.3
+    'voiceman_05300',
+    'luckmi101_03771', 'luckmi102_03772', 'luckmi103_03773', 'luckmi104_03774',
+    'gaiusc605_01005', 'luckmi105_03775', 'luckmi106_03776', 'luckmi107_03777',
+    'luckmi108_03778', 'luckmi109_03779', 'luckmi110_03780', 'luckmi111_03781',
+    'luckmi112_03782',
+    'voiceman_05301',
+    #  5.4
+    'voiceman_05400',
+    'luckmj101_04007', 'luckmj102_04008', 'luckmj103_04009', 'luckmj104_04010',
+    'luckmj105_04011', 'luckmj106_04012', 'luckmj107_04013', 'luckmj108_04014',
+    'luckmj109_04015', 'luckmj110_04016',
+    #  5.5
+    #   Part 1
+    'voiceman_05500',
+    'luckmk101_04058', 'luckmk102_04059', 'luckmk103_04060', 'luckmk104_04061',
+    'luckmk105_04062', 'luckmk106_04063',
+    #   Part 2
+    'voiceman_05505',
+    'luckml101_04064', 'luckml102_04065', 'luckml103_04066',
+    # 6.0 - Endwalker
+    'voiceman_06000',
+    'aktkma101_04357', 'aktkma102_04358',
+    'aktkma103_04359', 'aktkma104_04360', 'aktkma105_04361', 'aktkma106_04362',
+    'aktkma107_04363', 'aktkma108_04364', 'aktkma109_04365', 'aktkma110_04366',
+    'aktkma111_04367',
+    'aktkma112_04368', 'aktkma113_04369', 'aktkma114_04370', 'aktkma115_04371',
+    'aktkma116_04372', 'aktkma117_04373', 'aktkma118_04374', 'aktkma119_04375',
+    'aktkma120_04376',
+    'aktkma121_04377', 'aktkma122_04378', 'aktkma123_04379',
+    'voiceman_06001',
+    'aktkmb101_04380', 'aktkmb102_04381', 'aktkmb103_04382', 'aktkmb104_04383',
+    'aktkmb105_04384', 'aktkmb106_04385', 'aktkmb107_04386', 'aktkmb108_04387',
+    'aktkmb109_04388', 'aktkmb110_04389', 'aktkmb111_04390', 'aktkmb112_04391',
+    'aktkmb113_04392', 'aktkmb114_04393', 'aktkmb115_04394', 'aktkmb116_04395',
+    'aktkmb117_04396', 'aktkmb118_04397', 'aktkmb119_04398',
+    'voiceman_06002',
+    'aktkmc101_04399', 'aktkmc102_04400', 'aktkmc103_04401', 'aktkmc104_04402',
+    'aktkmc105_04403', 'aktkmc106_04404', 'aktkmc107_04405', 'aktkmc108_04406',
+    'aktkmc109_04407', 'aktkmc110_04408',
+    'voiceman_06003',
+    'aktkmd101_04409', 'aktkmd102_04410', 'aktkmd103_04411', 'aktkmd104_04412',
+    'aktkmd105_04413', 'aktkmd106_04414', 'aktkmd107_04415', 'aktkmd108_04416',
+    'aktkmd109_04417', 'aktkmd110_04418', 'aktkme101_04419',
+    'voiceman_06004',
+    'aktkme102_04420', 'aktkme103_04421', 'aktkme104_04422', 'aktkme105_04423',
+    'aktkme106_04424', 'aktkme107_04425', 'aktkme108_04426', 'aktkme109_04427',
+    'aktkme110_04428', 'aktkme111_04429', 'aktkme112_04430', 'aktkme113_04431',
+    'aktkme114_04432', 'aktkme115_04433', 'aktkme116_04434', 'aktkme117_04435',
+    'aktkme118_04436', 'aktkme119_04437', 'aktkme120_04438',
+    'voiceman_06005',
+    'aktkmf101_04439', 'aktkmf102_04440', 'aktkmf103_04441', 'aktkmf104_04442',
+    'aktkmf105_04443', 'aktkmf106_04444', 'aktkmf107_04445', 'aktkmf108_04446',
+    'aktkmf109_04447', 'aktkmf110_04448', 'aktkmf111_04449', 'aktkmg101_04450',
+    'voiceman_06006',
+    'aktkmg102_04451', 'aktkmg103_04452', 'aktkmg104_04453', 'aktkmg105_04454',
+    'aktkmg106_04455', 'aktkmg107_04456', 'aktkmg108_04457', 'aktkmg109_04458',
+    'aktkmg110_04459', 'aktkmg111_04460', 'aktkmg112_04461', 'aktkmg113_04462',
+    'aktkmg114_04463', 'aktkmg115_04464',
+    #  6.1
+    'voiceman_06100',
+    'aktkmh101_04526', 'aktkmh102_04527', 'aktkmh103_04528', 'aktkmh104_04529',
+    'aktkmh105_04530', 'aktkmh106_04531', 'aktkmh107_04532', 'aktkmh108_04533',
+    'aktkmh109_04534', 'aktkmh110_04535',
+    #  6.2
+    'voiceman_06200',
+    'aktkmi101_04592', 'aktkmi102_04593', 'aktkmi103_04594', 'aktkmi104_04595',
+    'aktkmi105_04596', 'aktkmi106_04597', 'aktkmi107_04598', 'aktkmi108_04599',
+    'aktkmi109_04600',
+    #  6.3
+    'voiceman_06300',
+    'aktkmj101_04670', 'aktkmj102_04671', 'aktkmj103_04672', 'aktkmj104_04673',
+    'aktkmj105_04674', 'aktkmj106_04675', 'aktkmj107_04676', 'aktkmj108_04677',
+    'aktkmj109_04678',
+    #  6.4
+    'voiceman_06400',
+    'aktkmk101_04735', 'aktkmk102_04736', 'aktkmk103_04737', 'aktkmk104_04738',
+    'aktkmk105_04739', 'aktkmk106_04740', 'aktkmk107_04741', 'aktkmk108_04742',
+    'aktkmk109_04743',
+    #  6.5
+    #   Part 1
+    'voiceman_06500',
+    'aktkml101_04744', 'aktkml102_04745', 'aktkml103_04746', 'aktkml104_04747',
+    'aktkml105_04748', 'aktkml106_04749', 'aktkml107_04750',
+    #   Part 2
+    'voiceman_06505',
+    'aktkmm101_04751', 'aktkmm102_04752', 'aktkmm103_04753',
 
     # Class/Job quests
     #  - GLD
@@ -257,9 +417,6 @@ $global:QUEST_INCLUDE_LIST = @(
     'clslnc000_00023', 'clslnc999_00180', 'clslnc998_00132', 'clslnc100_00218',
     'clslnc001_00047', 'clslnc002_00035', 'clslnc997_00143', 'clslnc003_00055',
     'clslnc004_00056', 'clslnc005_00438', 'clslnc006_00439',
-    #  - DRG
-    'jobdrg300_01067', 'jobdrg350_01068', 'jobdrg400_01069', 'jobdrg450_01070',
-    'jobdrg451_01071', 'jobdrg500_01072',
     #  - ARC
     'clsarc999_00181', 'clsarc998_00131', 'clsarc000_00021', 'clsarc100_00219',
     'clsarc001_00046', 'clsarc002_00067', 'clsarc997_00134', 'clsarc003_00068',
@@ -282,31 +439,57 @@ $global:QUEST_INCLUDE_LIST = @(
     'clsrog250_00148', 'clsrog300_00154', 'clsrog301_00155',
 
     #  - PLD
-    'jobpld300_01055', 'jobpld350_01056', 'jobpld400_01057', 'jobpld450_01058',
-    'jobpld451_01059', 'jobpld500_01060',
+    'jobpld300_01055', 'jobpld350_01056', 'jobpld400_01057',
+    'jobpld450_01058', 'jobpld451_01059', 'jobpld500_01060',
     #  - MNK
-    'jobmnk300_01061', 'jobmnk350_01062', 'jobmnk400_01063', 'jobmnk450_01064',
-    'jobmnk451_01065', 'jobmnk500_01066',
-    'jobmnk501_02026', 'jobmnk520_02027', 'jobmnk540_02028', 'jobmnk560_02029',
-    'jobmnk580_02030', 'jobmnk600_02031',
+    'jobmnk300_01061', 'jobmnk350_01062', 'jobmnk400_01063',
+    'jobmnk450_01064', 'jobmnk451_01065', 'jobmnk500_01066',
+    'jobmnk501_02026', 'jobmnk520_02027', 'jobmnk540_02028',
+    'jobmnk560_02029', 'jobmnk580_02030', 'jobmnk600_02031',
     #  - WAR
     'jobwar300_01049', 'jobwar350_01050', 'jobwar400_01051',
     'jobwar450_01052', 'jobwar451_01053', 'jobwar500_01054',
+    #  - DRG
+    'jobdrg300_01067', 'jobdrg350_01068', 'jobdrg400_01069',
+    'jobdrg450_01070', 'jobdrg451_01071', 'jobdrg500_01072',    
     #  - BRD
     'jobbrd300_01085', 'jobbrd350_01086', 'jobbrd400_01087',
     'jobbrd450_01088', 'jobbrd451_01089', 'jobbrd500_01090',
+    #  - WHM
+    'jobwhm300_01079', 'jobwhm350_01080', 'jobwhm400_01081',
+    'jobwhm450_01082', 'jobwhm451_01083', 'jobwhm500_01084',
     #  - DRK
     'jobdrk299_02110', 'jobdrk300_02053', 'jobdrk301_02054', 'jobdrk350_02055',
     'jobdrk400_02056', 'jobdrk450_02057', 'jobdrk500_02058',
+    'jobdrk501_02059', 'jobdrk520_02060', 'jobdrk540_02061',
+    'jobdrk560_02062', 'jobdrk580_02063', 'jobdrk600_02064',
+    'jobdrk601_02915', 'jobdrk630_02916', 'jobdrk650_02917',
+    'jobdrk680_02918', 'jobdrk700_02919', 'luckbc012_03221',
     #  - BLM
     'jobblm300_01073', 'jobblm350_01074', 'jobblm400_01075',
     'jobblm450_01076', 'jobblm451_01077', 'jobblm500_01078',
+    #  - SMN
+    'jobsmn300_01091', 'jobsmn350_01092', 'jobsmn400_01093',
+    'jobsmn450_01094', 'jobsmn451_01095', 'jobsmn500_01096',
     #  - SCH
     'jobsch300_01097', 'jobsch350_01098', 'jobsch400_01099',
     'jobsch450_01100', 'jobsch451_01101', 'jobsch500_01102',
     #  - SAM
     'jobsam500_02559', 'jobsam501_02560', 'jobsam520_02561', 'jobsam540_02562',
     'jobsam560_02563', 'jobsam580_02564', 'jobsam600_02565',
+    'jobsam601_02566', 'jobsam630_02567', 'jobsam650_02568', 'jobsam680_02569',
+    'jobsam700_02570',
+    #  - RDM
+    'jobrdm500_02576', 'jobrdm501_02577', 'jobrdm520_02578', 'jobrdm540_02579',
+    'jobrdm560_02580', 'jobrdm580_02581', 'jobrdm600_02582',
+    'jobrdm601_02583', 'jobrdm630_02584', 'jobrdm650_02585', 'jobrdm680_02586',
+    'jobrdm700_02587',
+    #  - GNB
+    'luckbb101_03261', 'luckbb102_03262', 'luckbb111_03263',
+    'luckbb121_03264', 'luckbb131_03265', 'luckbb141_03266',
+    #  - RPR
+    'aktkbb201_04073', 'aktkbb202_04074', 'aktkbb211_04075',
+    'aktkbb221_04076', 'aktkbb231_04077', 'aktkbb241_04078',
     #  - BLU
     'jobaoz001_03192', 'jobaoz010_03193', 'jobaoz100_03194', 'jobaoz200_03195',
     'jobaoz300_03196', 'jobaoz400_03197', 'jobaoz500_03198', 'jobaoz501_03199',
@@ -316,6 +499,20 @@ $global:QUEST_INCLUDE_LIST = @(
     'clsalc101_00580', 'clsalc150_00581', 'clsalc200_00582', 'clsalc250_00583',
     'clsalc300_00584', 'clsalc350_00647', 'clsalc400_00648', 'clsalc450_00649',
     'clsalc500_00650',
+
+    # Role quests
+    # - Shadowbringers
+    #   - Tank
+    'luckba101_03243', 'luckba111_03244', 'luckba121_03245',
+    'luckba131_03246', 'luckba141_03247', 'luckba151_03248',
+    #   - Healer
+    #   - Phys. DPS
+    'luckba301_03273', 'luckba311_03274', 'luckba321_03275',
+    'luckba331_03276', 'luckba341_03277', 'luckba351_03278',
+    #   - Mag. DPS
+    'luckba401_03623', 'luckba411_03624', 'luckba421_03625',
+    'luckba431_03626', 'luckba441_03627', 'luckba451_03628',
+    # - Endwalker
 
     # Primals
     'gaiusd001_01047', 'gaiusd002_01048', 'gaiusd003_01157', 'gaiusd004_01158',
@@ -328,16 +525,30 @@ $global:QUEST_INCLUDE_LIST = @(
     'gaiusd201_01199', 'gaiusx201_01709', 'gaiusd202_01200', 'gaiusd203_01201',
     'gaiusd204_01202', 'gaiusd205_01203', 'gaiusd401_01474', 'gaiusd601_00494',
     'gaiusd602_00495', 'gaiusd701_00497', 'mansea200_00546', 'gaiusd702_00498',
+    # The Shadow of Mhach
+    'heavnm101_02204', 'heavnm102_02205', 'heavnm201_02285', 'heavnm202_02286',
+    'heavnm301_02373', 'heavnm302_02374', 'heavnm303_01480',
 
+    # Dungeon unlock quests
+    'gaiusa602_00764', 'gaiusb812_00921', 'gaiusc404_00979', 'gaiusc702_01014',
+    'gaiusb501_00870', 'subfst900_01135',
+
+    # Side quests
+    'subgsc001_00434', 'gaiusa907_00802', 'subsea921_00699', 'subcts896_03017',
+    'subcts897_03018', 'subcts810_01431', 'subcts801_01210',
 
     # Seasonal Events
     #  - Heavensturn
     #    2023
     'fesnyr901_04588', 'fesnyr902_04589',
     #  - Valentione's Day
+    #    2024
+    'fesvlx001_04808', 'fesvlx002_04809',
     #    2023
     'fesvlt901_04654',
     #  - Little Ladies' Day
+    #    2024
+    'fespdy901_04810', 'fespdy902_04811',
     #    2023
     'fespdy801_04720',
     #    2022
@@ -348,27 +559,41 @@ $global:QUEST_INCLUDE_LIST = @(
     #    2022
     'fesest801_04590',
     #  - Make It Rain
+    #    2024
+    'fesgsc801_04814',
     #    2023
     'fesgsc701_04727', 'fesgsc702_04728',
     #    2022
     'fesgsc601_04584', 
     #  - Moonfire Faire
-    #    2022
-    'fessum801_04540', 'fessum802_04541',
     #    2023
     'fessum901_04723', 'fessum902_04724',
-    #  - Rising
+    #    2022
+    'fessum801_04540', 'fessum802_04541',
+    #  - The Rising
+    #    2023
+    'fesanv901_04765', 'fesanv902_04766',
     #    2022
     'fesanv801_04544',
     #  - All Saints' Wake
+    #    2023
+    'feshlw901_04785', 'feshlw902_04786',
     #    2022
     'feshlw801_04655', 'feshlw802_04656', 'feshlw803_04657',
     #  - Starlight Celebration
+    #    2023
+    'fesxms901_04781', 'fesxms902_04782', 'fesxms903_04783',
     #    2022
     'fesxms801_04658', 'fesxms802_04659',
     
-    #  - FFXI Collab 2022
+    #  - FFXVI Collab 2024
+    'fessxt101_04796', 'fessxt102_04797', 'fessxt103_04798',
+    #  - FFXV Collab 2019
+    'fesbkc001_03158', 'fesbkc002_03159', 'fesbkc003_03160',
+    #  - FFXI Collab 2015
     'fesevn101_02206',
+    #  - Yo-kai Watch
+    'fesykw001_02141',
 
     # Island Sanctuary
     'aktkua101_04643', 'aktkua102_04644', 'aktkua201_04645'
